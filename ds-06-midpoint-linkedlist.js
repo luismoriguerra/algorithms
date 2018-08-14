8fc8b8e1318b92c5ed66a2b3a22108195aac54a9
Isaac Luis <luismori@outlook.com>
2018-08-14 17:21:04 -0500

    Update ds-06-midpoint-linkedlist.js

--- ds-06-midpoint-linkedlist.js
+++ ds-06-midpoint-linkedlist.js
@@ -9,3 +9,38 @@ function midPoint(list) {
    }
   return slow;
 }
+
+function circularList (list) {
+  let slow = list.getFirst();
+  let fast = list.getFirst();
+  
+   while (fast.next && fast.next.next) {
+      slow = slow.next;
+      fast = fast.next.next;
+     
+     if (slow === fast) return true;
+   }
+  
+  return false;
+  
+}
+
+
+function fromLast(list, n) {
+  let slow = list.getFirst();
+  let fast = list.getFirst();
+  
+   while(n > 0) {
+      fast = fast.next;
+     n--;
+   }
+  
+  while(fast.next) {
+    slow = slow.next;
+    fast = fast.next;
+  }
+   
+  return slow;
+  
+  
+}
