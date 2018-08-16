2b422d9c0ccbb15835fed4217e9ac24b902d60cb
Isaac Luis <luismori@outlook.com>
2018-08-15 19:33:34 -0500

    Update countingDuplicate.js

--- countingDuplicate.js
+++ countingDuplicate.js
@@ -1,4 +1,3 @@
 function duplicateCount(text){
-  const matches = text.toLocaleLowerCase().split('').sort().join('').match(/(.+)\1/g);
-  return matches && matches.length || 0
+  return (text.toLocaleLowerCase().split('').sort().join('').match(/(.+)\1/g) || []).length
 }
