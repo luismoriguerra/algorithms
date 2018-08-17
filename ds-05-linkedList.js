5291077ef8bd0d3fce28b35553ecac09b85b1bda
Isaac Luis <luismori@outlook.com>
2018-08-16 19:12:48 -0500

    Update ds-05-linkedList.js

--- ds-05-linkedList.js
+++ ds-05-linkedList.js
@@ -129,3 +129,50 @@ class LinkedList {
         }
     }
 }
+
+
+function midPoint(list) {
+  let slow = list.getFirst();
+  let fast = list.getFirst();
+   
+   while (fast.next && fast.next.next) {
+      slow = slow.next;
+      fast = fast.next.next
+   }
+  return slow;
+}
+
+function circularList (list) {
+  let slow = list.getFirst();
+  let fast = list.getFirst();
+  
+   while (fast.next && fast.next.next) {
+      slow = slow.next;
+      fast = fast.next.next;
+     
+     if (slow === fast) return true;
+   }
+  
+  return false;
+  
+}
+
+
+function fromLast(list, n) {
+  let slow = list.getFirst();
+  let fast = list.getFirst();
+  
+   while(n > 0) {
+      fast = fast.next;
+     n--;
+   }
+  
+  while(fast.next) {
+    slow = slow.next;
+    fast = fast.next;
+  }
+   
+  return slow;
+  
+  
+}
